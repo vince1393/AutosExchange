080f3d822259e522c1e433214fc5d9ff37b3cad9
Vincent Romani <vromani@outlook.com>
2019-08-08 19:50:50 -0400

    initial commit

---
+++ Controllers/ListingsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AutosExchange.Models;
+
+namespace AutosExchange.Controllers
+{
+    public class ListingsController : Controller
+    {
+        private AutosExchangeEntities db = new AutosExchangeEntities();
+
+        // GET: Listings
+        public ActionResult Index()
+        {
+            var listings = db.Listings.Include(l => l.Lister);
+            return View(listings.ToList());
+        }
+
+        // GET: Listings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Listing listing = db.Listings.Find(id);
+            if (listing == null)
+            {
+                return HttpNotFound();
+            }
+            return View(listing);
+        }
+
+        // GET: Listings/Create
+        public ActionResult Create()
+        {
+            ViewBag.ListerId = new SelectList(db.Listers, "ListerId", "Name");
+            return View();
+        }
+
+        // POST: Listings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ListingId,ListerId,VIN,Year,Make,Model,Trim,KM,isNewCar,CarType,Color,Drivetrain,FuelType,Transmission,FuelEconomy,isAvailable,SoldPrice")] Listing listing)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Listings.Add(listing);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ListerId = new SelectList(db.Listers, "ListerId", "Name", listing.ListerId);
+            return View(listing);
+        }
+
+        // GET: Listings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Listing listing = db.Listings.Find(id);
+            if (listing == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ListerId = new SelectList(db.Listers, "ListerId", "Name", listing.ListerId);
+            return View(listing);
+        }
+
+        // POST: Listings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ListingId,ListerId,VIN,Year,Make,Model,Trim,KM,isNewCar,CarType,Color,Drivetrain,FuelType,Transmission,FuelEconomy,isAvailable,SoldPrice")] Listing listing)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(listing).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ListerId = new SelectList(db.Listers, "ListerId", "Name", listing.ListerId);
+            return View(listing);
+        }
+
+        // GET: Listings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Listing listing = db.Listings.Find(id);
+            if (listing == null)
+            {
+                return HttpNotFound();
+            }
+            return View(listing);
+        }
+
+        // POST: Listings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Listing listing = db.Listings.Find(id);
+            db.Listings.Remove(listing);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutosExchange.Startup))]
+namespace AutosExchange
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
